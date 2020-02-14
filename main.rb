04080b26a9afc9add6a21e05603746952fd4f2df
André Luís Jorge Paulino <PUCMINAS\1038051@PPC01INFLL00114.pucminas.net>
2020-02-13 22:10:09 -0300

    #1: Tres tentativas implementada.

--- main.rb
+++ main.rb
@@ -1,7 +1,23 @@
-puts "Entre com um palpite: "
-palpite = gets.chomp.to_i
+puts "Olá, você tem 3 palpites!"
+
+puts "Entre com o primeiro palpite: "
+palpite1 = gets.chomp.to_i
+
+puts "Entre com o segundo palpite: "
+palpite2 = gets.chomp.to_i
+
+puts "Entre com o terceiro palpite: "
+palpite3 = gets.chomp.to_i
 
 numero_secreto = 121
 
 puts "Você acertou?"
-puts palpite == numero_secreto
+if palpite1 == numero_secreto
+    puts palpite1 == numero_secreto; puts "Você acertou no primeiro palpite!"
+elsif palpite2 == numero_secreto 
+    puts palpite2 == numero_secreto; puts "Você acertou no segundo palpite!"
+elsif palpite3 == numero_secreto 
+puts palpite3 == numero_secreto; puts "Você acertou no terceiro palpite!"
+else 
+    puts "Você errou todos os palpites."
+end
